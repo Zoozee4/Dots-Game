9c6934bf14d981d3e8007d21a1ad51fd3597aa22
Zoozee4 <zoozee4@windowslive.com>
2016-08-01 22:28:19 +0300

    Progress 2.1
    Reconfigured the locator method, so it shouldn't allow the player to place a dot in the same place twice.

--- Controller.java
+++ Controller.java
@@ -4,27 +4,27 @@ import java.io.*;
 public class Controller {
 	Scanner Input = new Scanner(System.in);
 	Grid grid;
-	
-	public int preset;		//Change to anything other that 9 if you want to change the scale.
+
+	public int preset;
 	public int scale;
 	public int gamemode;
 	public int currentPlayer;
 	public String [] [] dotsList;
 	public boolean gameover = false;
-	
+
 	public Controller () {
 		grid = new Grid();
-		preset = 0;
+		preset = 9;				//Change to anything other that 9 if you want to change the scale.
 		scale = setScale();
 		dotsList = initMatrix();
 		//gamemode = setGamemode();
 	}
-	
+
 	public int setScale() { 	
-		
+
 		if (preset != 9)
 		{
-			while (scale < 5 || scale > 12) 
+			while (scale < 5 || scale > 99) 
 			{
 				System.out.print("What would you like the scale to be (5 - 12)? ");
 				scale = Input.nextInt();
@@ -34,48 +34,56 @@ public class Controller {
 		else
 			return preset;
 	}
-	
+
 	public int setGamemode() {
 		int gamemode = 0;
-		
+
 		while (gamemode != 1 && gamemode != 2) 
 		{
-		System.out.print("Choose a gamemode 1) Singleplayer, 2) Multiplayer : ");
-		gamemode = Input.nextInt();
-		this.gamemode = gamemode;
+			System.out.print("Choose a gamemode 1) Singleplayer, 2) Multiplayer : ");
+			gamemode = Input.nextInt();
+			this.gamemode = gamemode;
 		}
 		return this.gamemode;
 	}
-	
+
 	public String [] [] initMatrix() {
-		
+
 		String [] [] dotsList = new String [scale] [scale];
-		
+
 		for (int rows = 0; rows < scale; rows ++)
 			for (int columns = 0; columns < scale; columns ++)
 				dotsList [rows][columns] = " ";
 		return dotsList;
 	}
-	
+
 	public void locator() {
 		int x = 0;
 		int y = 0;
+		boolean check = false;
+
 		
-		System.out.println("\n\nType in coordinates to place your dot.");
-		while (x < 1 || x > scale)
+		while (check == false)
 		{
-		System.out.print("X : ");
-		x = Input.nextInt();
+			System.out.println("\n\nType in coordinates to place your dot.");
+			
+			while (x < 1 || x > scale)
+			{
+				System.out.print("X : ");
+				x = Input.nextInt();
+			}
+			while (y < 1 || y > scale)
+			{
+				System.out.print("Y : ");
+				y = Input.nextInt();
+			}
+			x -= 1;
+			y -= 1;
+			if (dotsList [x] [y] != "O")
+			{
+				dotsList [x] [y] = "O";
+				check = true;
+			}
 		}
-		while (y < 1 || y > scale)
-		{
-		System.out.print("Y : ");
-		y = Input.nextInt();
-		}
-		
-		x -= 1;
-		y -= 1;
-		dotsList [x] [y] = "O";
 	}
-	
 }
