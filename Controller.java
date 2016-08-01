d4cd9744de0c74d8753e104d2877f759b14a6418
Zoozee4 <zoozee4@windowslive.com>
2016-08-01 11:21:11 +0300

    Progress 1
    The grid class now asks for the gamemode -either 1 or 2- and it also asks for a grid scale -which could be preset to 9- and displays it correctly.
     
    The controller class has a bunch of new objects along with a couple of setters.

--- Controller.java
+++ Controller.java
@@ -2,13 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Controller {
-	
-	public Controller() {	
-	int [] [] dotsList = new int[0][0];
-	int currentPlayer;
+	Scanner Input = new Scanner(System.in);
+
+	public int scale;
+	public int gamemode;
+	public int currentPlayer;
+	public String [] [] dotsList = new String [scale] [scale];
+
+	public int setScale(int preset) {
+		if (preset != 9)
+		{
+			while (scale < 5 || scale > 12) 
+			{
+				System.out.print("What would you like the scale to be (5 - 12)? ");
+				scale = Input.nextInt();
+			}
+			return scale;
+		}
+		else
+			return preset;
 	}
 	
-	public static void main(String [] args) {
+	public int setGamemode() {
+		int gamemode = 0;
 		
+		while (gamemode != 1 && gamemode != 2) 
+		{
+		System.out.print("Choose a gamemode 1) Singleplayer, 2) Multiplayer : ");
+		gamemode = Input.nextInt();
+		this.gamemode = gamemode;
+		}
+		return this.gamemode;
 	}
 }
--- Grid.java
+++ Grid.java
@@ -1,41 +1,58 @@
 
 public class Grid {
+
+	public static String [] [] initMatrix(int scale, String [] [] dotsList) {
+
+		for (int rows = 0; rows < scale; rows ++)
+			for (int columns = 0; columns < scale; columns ++)
+				dotsList [rows][columns] = "X";
+		return dotsList;
+	}
+
+	public static void gridDisplay(int scale, String [] [] dotsList) {
+
+		int top = 4 * (scale + 1);
+		int middle = 2 + scale;
+		int intersection = 1 + scale;
+		int bottom = 1 + scale;
+
+		System.out.print(" ");
+		for (int a = 0; a < top - 1; a ++)
+			System.out.print("_");
+		
+		for (int b = 0; b < scale; b ++)
+		{
+		System.out.println(" ");
+
+		for (int c = 0; c < middle - 1; c ++)
+			System.out.print("|   ");
+		System.out.print("|");
+		
+		System.out.println(" ");
+
+		System.out.print("|");
+		for (int d = 0; d < intersection - 1; d ++)
+			System.out.print("---" + dotsList[d][b]);
+		System.out.print("---|");
+		}
+		
+		System.out.println(" ");
+		
+		for (int e = 0; e < bottom; e ++)
+			System.out.print("|___");
+		System.out.print("|");
+		
+	}
+
 	public static void main(String [] args) {
-
-
-		int scale = 9;
+		Controller object = new Controller();
+		int scale = object.setScale(9);		//Change 9 to anything if you want to change the scale.
+		int gamemode;
 		String [] [] dotsList = new String [scale][scale];
 		
-		for (int a = 0; a < scale; a ++)
-			for (int b = 0; b < scale; b ++)
-				dotsList[a][b] = "-X--";
-		
-		for (int a = 0; a < scale; a ++) {
-			System.out.print("\n|--");
-			for (int b = 0; b < scale; b ++)
-				System.out.print(dotsList[a][b]);
-		}
-		/*
-		System.out.println(" _______________________________________");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|   |   |   |   |   |   |   |   |   |   |");
-		System.out.println("|---|---|---|---|---|---|---|---|---|---|");
-		System.out.println("|___|___|___|___|___|___|___|___|___|___|");
-		 */
+
+		object.dotsList = initMatrix(scale, dotsList);
+		gamemode = object.setGamemode();
+		gridDisplay(scale, object.dotsList);
 	}
 }
